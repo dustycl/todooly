eeecb8ecfd6ba37a5a649aff809e850e238b5782
dustycl <dustycl@gmail.com>
2020-09-14 12:31:06 -0700

    Added list loading from array

--- list.js
+++ list.js
@@ -1,16 +1,28 @@
-const list = document.getElementById("list");
-
+const input = document.getElementById("input-text");
 const addBtn = document.getElementById("add-item");
 
+const userList =["laundry", "dishes", "homework", "groceries"];
+
+const list = document.getElementById("list");
+for (let i = 0; i < userList.length; i++) {
+    let text = userList[i];
+    addItem(text);
+}
+
 addBtn.addEventListener('click', () => {
-  const input = document.getElementById("input-text");
   let text = input.value;
 
   if (text !== "") {
+    addItem(text);
+  }
+});
+
+function addItem(text) {
     const item = document.createElement("li");
     item.id = text;
     let deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
+
     deleteBtn.addEventListener('click', (e) => {
       e.currentTarget.parentNode.remove();
     });
@@ -18,5 +30,4 @@ addBtn.addEventListener('click', () => {
     item.textContent = text.toLowerCase();
     item.appendChild(deleteBtn);
     list.appendChild(item);
-  }
-});
+}
